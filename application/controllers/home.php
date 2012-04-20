a4cf341040e0c7985f60b67e1a229c49126e0063
Tom Houdmont <tomhoudmont@gmail.com>
2012-04-20 15:42:51 +0100

    Display views in a template fashion

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -25,6 +25,13 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('home_view');
+		//$this->load->view('templates/page_view', $data);
+	}
+
+	public function test() {
+		$data['header'] = array('title' => 'Blue bar title');
+
+		$this->load->view('templates/page_view', $data);
 	}
 }
 
---
+++ application/views/templates/page_view.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('templates/header_view'); ?>
+
+This is a test. Please ignore.
+
+<?php $this->load->view('templates/footer_view'); ?>
